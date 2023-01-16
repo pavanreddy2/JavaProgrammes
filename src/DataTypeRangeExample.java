4beef4c3d69ac1802fd3100119bb571a15dd4f25
pavan0229 <bugganapavan@gmail.com>
2023-01-16 20:12:43 +0530

    Adding files

---
+++ src/DataTypeRangeExample.java
@@ -0,0 +1,16 @@
+
+public class DataTypeRangeExample {
+
+	public static void main(String[] args) {
+
+	
+		
+     System.out.println(Byte.MIN_VALUE +"----> "+Byte.MAX_VALUE);
+     System.out.println(Short.MIN_VALUE +"----> "+Short.MAX_VALUE);
+     System.out.println(Integer.MIN_VALUE +"----> "+Integer.MAX_VALUE);
+     System.out.println(Long.MIN_VALUE +"----> "+Long.MAX_VALUE);
+     System.out.println(Character.MIN_VALUE +"----> "+Character.MAX_VALUE);
+
+	}
+
+}
---
+++ src/Example4.java
@@ -0,0 +1,12 @@
+
+public class Example4 {
+ public static void main(String[] args) {
+	
+	 int a = 120;  //
+	 if (a % 2 != 0) {   //1 != 0
+		  System.out.println( a + " is even number ");
+	 } else {
+		  System.out.println(a + " is odd number     ");
+	 }
+}
+}
